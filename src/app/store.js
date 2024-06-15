bdd0be416776e5ca86a1b207ca5ed7789813addb
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2024-06-15 13:14:53 +0530

    removed payment slice and added order update after verifing order in online payment

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 // import counterReducer from '../features/counter/counterSlice';
-import productSlice from '../features/product-list/productSlice';
-import authSlice from '../features/auth/authSlice'
-import cartSlice from '../features/cart/cartSlice';
-import orderSlice from '../features/order/orderSlice';
-import paymentSlice from '../features/payment.js/paymentSlice';
+import productSlice from "../features/product-list/productSlice";
+import authSlice from "../features/auth/authSlice";
+import cartSlice from "../features/cart/cartSlice";
+import orderSlice from "../features/order/orderSlice";
 
 export const store = configureStore({
   reducer: {
@@ -12,6 +11,5 @@ export const store = configureStore({
     auth: authSlice,
     cart: cartSlice,
     order: orderSlice,
-    payment: paymentSlice
   },
 });
--- src/features/order/orderApi.js
+++ src/features/order/orderApi.js
@@ -1,49 +1,53 @@
 export async function createOrder(data) {
-
-  const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/pay/create-order`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-  const d = await response.json(); 
+  const response = await fetch(
+    `${process.env.REACT_APP_BACKEND_URL}/pay/create-order`,
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const d = await response.json();
   return new Promise((resolve, reject) => {
-    resolve(d)
-  })
-
+    resolve(d);
+  });
 }
 
 export async function getOrders(id) {
-
   // const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/orders?user=` + user)
 
-  const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/order/all/`+id);
-  const d = await response.json()
+  const response = await fetch(
+    `${process.env.REACT_APP_BACKEND_URL}/order/all/` + id
+  );
+  const d = await response.json();
   return new Promise((resolve, reject) => {
-    resolve(d)
-  })
-
+    resolve(d);
+  });
 }
 
 export async function getOrderById(id) {
-  const res = await fetch('http://localhost:5000/order/' + id)
-  const d = await res.json()
+  const res = await fetch("http://localhost:5000/order/" + id);
+  const d = await res.json();
   return new Promise((resolve, reject) => {
-    resolve(d)
-  })
+    resolve(d);
+  });
 }
 
-export async function updateOrder({id,update}) {
-  const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/order/update/`+id, {
-    method: "PATCH",
-    body: JSON.stringify(update),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-  const d = await response.json(); 
+export async function updateOrder({ id, update }) {
+  const response = await fetch(
+    `${process.env.REACT_APP_BACKEND_URL}/order/update/` + id,
+    {
+      method: "PATCH",
+      body: JSON.stringify(update),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const d = await response.json();
   return new Promise((resolve, reject) => {
-    resolve(d)
-  })
+    resolve(d);
+  });
 }
