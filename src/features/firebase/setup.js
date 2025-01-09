641de5d31e2babcd4f27545b50b01dd99cd35e1d
Vaibhav bagate <72258603+vaibhav25-mnnit@users.noreply.github.com>
2025-01-09 21:44:28 +0530

    Update setup.js

--- src/features/firebase/setup.js
+++ src/features/firebase/setup.js
@@ -8,10 +8,10 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_FIREBASE_APIKEY,
   authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_FIREBASE_APPID,
+  projectId: process.env.REACT_FIREBASE_PROJECTID,
   storageBucket: process.env.REACT_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.,
+  appId: process.env.REACT_FIREBASE_APPID,
 };
 
 // Initialize Firebase
