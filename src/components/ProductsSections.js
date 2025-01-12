b8e43cd2799eeb944d383e2a2398301ab00144b5
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2025-01-12 15:21:49 +0530

    fixed scroll bug for products Section in Homepage

--- src/components/ProductsSections.js
+++ src/components/ProductsSections.js
@@ -20,18 +20,21 @@ function ProductsSections({ tittle, tag }) {
     <>
       <div className="bg-white flex py-4 flex-col items-center shadow-white dark:bg-zinc-950 dark:text-white">
         <hr className="border-gray-200 sm:mx-auto" />
+
         <h1 className="text-2xl uppercase font-semibold tracking-tight my-4  sm:my-10 sm:text-4xl">
           {tittle}
         </h1>
-        {/* <div className="flex w-full gap-x-3 p-4 overflow-x-auto"></div> */}
+
         {showLoading === true ? (
           <Loader />
         ) : (
           <>
-            <div className="flex overflow-auto justify-center p-5 gap-5 w-[90%] md:1/2 lg:w-2/3 ">
-              {products.map((product) => (
-                <ProductCard product={product} />
-              ))}
+            <div className="flex justify-center w-full">
+              <div className="flex overflow-x-auto gap-4 p-4 ">
+                {products.map((product) => (
+                  <ProductCard product={product} />
+                ))}
+              </div>
             </div>
           </>
         )}
